a0adb999367414dc19690086128f68c189835c8e
kivas1k <kivis006@gmail.com>
2025-05-28 09:59:45 +0700

    Добавлен интерактивный main.py для ToDo App

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
 from todo import add_task, remove_task, list_tasks, mark_done
 
-print(add_task("Сделать домашку"))
-print(add_task("Почитать книгу"))
-print(mark_done("Сделать домашку"))
-print(remove_task("Почитать книгу"))
-print("\nСписок задач:")
-print("\n".join(list_tasks()))
+def main():
+    while True:
+        print("\nToDo App: 1. Добавить задачу 2. Удалить задачу 3. Отметить выполненной 4. Показать задачи 5. Выход")
+        choice = input("Выберите действие (1-5): ")
+        if choice == "1":
+            task = input("Введите задачу: ")
+            print(add_task(task))
+        elif choice == "2":
+            task = input("Введите задачу для удаления: ")
+            print(remove_task(task))
+        elif choice == "3":
+            task = input("Введите задачу для отметки как выполненной: ")
+            print(mark_done(task))
+        elif choice == "4":
+            print("\nСписок задач:")
+            print("\n".join(list_tasks()))
+        elif choice == "5":
+            print("Выход")
+            break
+        else:
+            print("Неверный выбор, попробуйте снова")
+
+if __name__ == "__main__":
+    main()
